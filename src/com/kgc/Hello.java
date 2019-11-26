2a5146472bab638bc0568d9f28e64dbf45e100d4
zhangsan <zhangsan@kgc.cn>
2019-11-26 15:16:48 +0800

    第一次提交

---
+++ src/com/kgc/Hello.java
@@ -0,0 +1,11 @@
+package com.kgc;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("commit");
+        System.out.println("commit");
+        System.out.println("commit");
+        System.out.println("commit");
+        System.out.println("commit");
+    }
+}
